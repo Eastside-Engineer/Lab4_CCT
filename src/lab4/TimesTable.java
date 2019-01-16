978412a0b90867c89ec85e9f6e9c096ad04b0506
Cynthia <cturner@ltu.edu>
2019-01-16 16:47:21 -0500

    Lab update

--- src/lab4/TimesTable.java
+++ src/lab4/TimesTable.java
@@ -7,21 +7,53 @@ public class TimesTable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String choice = ("y");// allows users to pick Y to continue
-		
-		while (choice.equalsIgnoreCase("y")) {//This allows input to take any case of y
-		
 		Scanner scnr = new Scanner(System.in);
-	
-		}do {// do - loop to make user pick between 1-100.
-				System.out.println("Give me a number: ");
-				userinput = scnr.nextInt();
-			
-			}
-			System.out.println("Continue: Press Y or any key to exit");//Allows user to exit program
-			choice = scnr.next();
-	}
+		
+		String input = "y";
+		
+		
+		
+		while (input.equalsIgnoreCase("y")){ 
+		
+		
+		System.out.println("Enter an integer: ");
+		int num = scnr.nextInt();
+		
+		System.out.printf("%-22s%-22s%-22s\n","Number ","Squared ","Cubed ");
+		System.out.printf("%-22s%-22s%-22s\n","~~~~~~ ","~~~~~~","~~~~~~");
+		
+		for (int i = 1; i <= num; i++) {
+			System.out.printf("%-22d%-22d%-22d\n",i, i*i, i*i*i);
+			System.out.println();
+		}
+		
+		System.out.println("Continue?: (y/n)");//Allows user to exit program
+		input = scnr.next();
+		}
+		
+		System.out.println("Multiplication table?: (y/n)");
+		String mood = scnr.next();
+		
+		while (mood.equalsIgnoreCase("y")){ 
+
+		System.out.println("PICK A NUMBER!: ");
+		int num2 = scnr.nextInt();
+		
+		for (int j=1; j <= num2; j++){
+		    for (int k=1; k <= num2; k++){ 
+		    	System.out.print(j*k + "\t");
+		    }
+		    System.out.println();
+		    System.out.println();
+		}
+		System.out.println("Continue? (y/n)");
+		mood = scnr.next();
+		}    
+		
+		System.out.println("GET AWAY FROM ME!");
 		scnr.close();
+
 	
 	}
-}
+	}
+
